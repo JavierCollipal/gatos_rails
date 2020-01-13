fbba04b444f0d76b93511e2359bc045f22d32d7d
JavierCollipal <jcollipal1212@gmail.com>
2020-01-13 13:25:50 -0300

    fix to cat model

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,4 @@
 class Cat < ApplicationRecord
   validates :name, presence: true, length: { minimum: 4, maximum: 200}, uniqueness: true
-  validates :page, presence: true, length: { in: 0...20}
+  validates :age, presence: true, length: { in: 0...20}
 end
