953520cb0c4d2ce2c8a04b3451c0253a9647760e
JavierCollipal <jcollipal1212@gmail.com>
2020-01-20 15:44:32 -0300

    validations added to model

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,3 +1,4 @@
 class Breed < ApplicationRecord
   has_one :cat
+  validates :name, presence: true, uniqueness: true, length: 50
 end
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,9 @@
 class Cat < ApplicationRecord
   belongs_to :color
   belongs_to :breed
+  validates :name, presence: true, uniqueness: true, length: 50
+  validates :age, numericality:  { only_integer: true, less_than_or_equal_to: 20}
+  validates :color_id, presence: true, numericality: { only_integer: true}
+  validates :color_id, presence: true, numericality: { only_integer: true}
+  validates :breed_id, presence: true, numericality: {only_integer: true}
 end
