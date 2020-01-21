19a863ca8085edd6d59d514e464129751860bc3e
JavierCollipal <jcollipal1212@gmail.com>
2020-01-21 11:17:22 -0300

    little fix to cat model

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -4,6 +4,5 @@ class Cat < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { minimum: 1, maximum: 50}
   validates :age, numericality:  { only_integer: true, less_than_or_equal_to: 20}
   validates :color_id, presence: true, numericality: { only_integer: true}
-  validates :color_id, presence: true, numericality: { only_integer: true}
   validates :breed_id, presence: true, numericality: {only_integer: true}
 end
